e94ed10dc40c3ec0b5cf0b456643acf39f5615cd
Rustam Yuldashev <r.uldashev@gmail.com>
2017-12-26 18:00:37 +0300

    Modified function "shorten" . Now we use three "tables" (actually pseudo-folders): - table#1 - hash of long url as key with short url as value for avoid duplicates - table#2 - short url as key with empty value for check of uniqueness of short url - table#3 - short url as key with long url as value for reverse look up When we recieved url for shorten we count hash of url, then check if hash of url exists in table#1, if yes we return short url, if not we generate unique short url and write it to table#2, and then atomically write information into table# 1 and table#3
    TODO
    I have suggestion, that table#2 unnecessary....

--- tinyfix.py
+++ tinyfix.py
@@ -2,38 +2,76 @@
 
 import etcd3
 import flask
+import hashlib
 import random
 import re
 import string
-import time
 import wtforms
 
 
 etcd = etcd3.client()
 FQDN = 'localhost'
 SHORT_URL_RE = re.compile('^[-_a-zA-Z0-9]{4,8}$')
+HASH_STRING = '-_' + string.letters + string.digits
 
 
-def gen_hash():
-    hash_string = '-_' + string.letters + string.digits
-    random.seed(time.time())
+def gen_short_url():
+    while True:
+        short_url = ''.join(random.choice(HASH_STRING) for _ in xrange(random.randint(4, 8)))
+        if etcd.get('/short_url/{}'.format(short_url)) == (None, None):
+            etcd.put('/short_url/{}'.format(short_url), '')
+            break
 
-    return ''.join(random.choice(hash_string) for _ in xrange(random.randint(4, 8)))
+    return short_url
+
+
+def count_hash(url):
+    return hashlib.sha256(url).hexdigest()
 
 
 def shorten(url):
     my_domain = 'http://{}/{}'
-    while True:
-        url_hash = gen_hash()
-        if etcd.get(url_hash) == (None, None):
-            etcd.put(url_hash, url)
-            break
+    hash_url = count_hash(url)
+    short_url, _ = etcd.get('/hash_url/short_url/{}'.format(hash_url))
 
-    return my_domain.format(FQDN, url_hash)
+    if short_url is None:
+        short_url = gen_short_url()
+        transaction_status, responses = etcd.transaction(
+            compare=[
+                etcd.transactions.version('/hash_url/short_url/{}'.format(hash_url)) == 0,
+                etcd.transactions.version('/short_url/long_url/{}'.format(short_url)) == 0,
+            ],
+            success=[
+                etcd.transactions.put('/hash_url/short_url/{}'.format(hash_url), short_url),
+                etcd.transactions.put('/short_url/long_url/{}'.format(short_url), url),
+            ],
+            failure=[
+                etcd.transactions.get('/hash_url/short_url/{}'.format(hash_url)),
+                etcd.transactions.get('/short_url/long_url/{}'.format(short_url)),
+            ]
+        )
+
+        if not transaction_status:
+            raise ValueError(
+                '\n\t{msg00}: {ts}, {msg01}: {r};\n\t{msg1}: {h_url}={s_url1};\n\t{msg2}: {s_url2}={l_url}'.format(
+                    msg00='transaction status',
+                    ts=transaction_status,
+                    msg01='responses',
+                    r=responses,
+                    msg1='hash_url=short_url kv entry',
+                    h_url=hash_url,
+                    s_url1=etcd.get('/hash_url/short_url/{}'.format(hash_url))[0],
+                    msg2='short_url=long_url kv entry',
+                    s_url2=short_url,
+                    l_url=etcd.get('/short_url/long_url/{}'.format(short_url))[0],
+                )
+            )
+
+    return my_domain.format(FQDN, short_url)
 
 
 def unshorten(path):
-    long_url = etcd.get(path)[0]
+    long_url, _ = etcd.get('/short_url/long_url/{}'.format(path))
 
     return False if long_url is None else long_url
 
@@ -77,7 +115,7 @@ def find_and_return(short_url):
     if flask.request.host.split(':')[0] != FQDN:
         return 'Unknown HOST header'
 
-    if not SHORT_URL_RE(short_url):
+    if not SHORT_URL_RE.match(short_url):
         return 'Unknown URL format'
 
     full_url = unshorten(path=short_url)
